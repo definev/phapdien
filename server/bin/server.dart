3a8f9790df3e28011d56a48555e1f9127c3e1cc9
Definev <daiduong.workmail@gmail.com>
2023-11-27 13:38:23 +0700

    Update server.dart and phapdien_crawler.dart

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
-
+import 'package:html/dom.dart';
+import 'package:html/parser.dart' as html;
+import 'package:http/http.dart' as http;
 import 'package:server/v0/routes/phapdien/list.dart';
 import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 // Configure routes.
@@ -20,17 +20,42 @@ Response _echoHandler(Request request) {
   return Response.ok('$message\n');
 }
 
-void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+// void main(List<String> args) async {
+//   // Use any available host or container IP (usually `0.0.0.0`).
+//   final ip = InternetAddress.anyIPv4;
 
-  // Configure a pipeline that logs requests.
-  final handler = Pipeline() //
-      .addMiddleware(logRequests())
-      .addHandler(_router);
+//   // Configure a pipeline that logs requests.
+//   final handler = Pipeline() //
+//       .addMiddleware(logRequests())
+//       .addHandler(_router);
 
-  // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+//   // For running in containers, we respect the PORT environment variable.
+//   final port = int.parse(Platform.environment['PORT'] ?? '8080');
+//   final server = await serve(handler, ip, port);
+//   print('Server listening on port ${server.port}');
+// }
+void printAllElementsText(Document document) {
+  // Select all script and style elements
+  var scriptsAndStyles = document.querySelectorAll('script, style');
+
+  // Remove each selected element
+  for (var element in scriptsAndStyles) {
+    element.remove();
+  }
+
+  // Iterate over all elements
+  for (var element in document.body!.nodes) {
+    if (element is Element) {
+      // Print the text content of the element
+      print(element.text);
+    }
+  }
+}
+void main() async {
+  final response = await http.get(
+    Uri.parse('https://vbpl.vn/tw/Pages/vbpq-print.aspx?dvid=13&ItemID=18562'),
+  );
+  final document = html.parse(response.body);
+  printAllElementsText(document);
+  print(document);
 }
--- server/lib/v0/apllications/phapdien_crawler.dart
+++ server/lib/v0/apllications/phapdien_crawler.dart
@@ -1,12 +1,35 @@
 // https://phapdien.moj.gov.vn/TraCuuPhapDien/KQPD/ActionHandler.aspx?do=taodanhmucvbkqpd&demucid=55323c64-e78f-4537-afcd-6a3c2af3c71d
-enum PhapdienCrawlerType {
-  actionHandler('ActionHandler.aspx'),
-  treeBoPD('TreeBoPD.aspx'),
-  ;
+sealed class PhapdienCrawlerType {
+  const PhapdienCrawlerType();
+}
 
-  const PhapdienCrawlerType(this.type);
-  final String type;
+// enum PhapdienCrawlerType {
+//   actionHandler('ActionHandler.aspx'),
+//   treeBoPD('TreeBoPD.aspx'),
+//   ;
 
+//   const PhapdienCrawlerType(this.type);
+//   final String type;
+// }
+
+class PhapdienCrawlerGetNode implements PhapdienCrawlerType {
+  const PhapdienCrawlerGetNode({
+    this.lenmpc = 20,
+  });
+
+  final String type = 'ActionHandler.aspx';
+  final String doType = 'loadnodes';
+  final int lenmpc;
+}
+
+class PhapdienCrawlerGetTree implements PhapdienCrawlerType {
+  const PhapdienCrawlerGetTree({
+    this.lenmpc = 20,
+  });
+
+  final String type = 'TreeBoPD.aspx';
+  final String doType = 'taodanhmucvbkqpd';
+  final int lenmpc;
 }
 
 /// https://phapdien.moj.gov.vn/TraCuuPhapDien/ActionHandler.aspx?do=loadnodes&lenmpc=20
@@ -14,5 +37,6 @@ class PhapdienCrawler {
   const PhapdienCrawler({
     required this.rootUrl,
   });
+
   final String rootUrl;
 }

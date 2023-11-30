43f88d0d1161e27776e86cccfed63289e383306f
Definev <daiduong.workmail@gmail.com>
2023-12-01 01:38:18 +0700

    add crawling data script

---
+++ server/bin/crawl_data.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:server/v0/domain/phapdien_node.dart';
+
+void main() async {
+  await crawlingData();
+}
+
+Future<void> crawlingData() async {
+  const sourceUrl = 'http://localhost:7777';
+
+  final demucContent = await http.get(Uri.parse('$sourceUrl/v0/phapdien/root'));
+
+  final list = json.decode(demucContent.body) as List<dynamic>;
+  final phapdienNodes = list //
+      .map((l) => PhapdienNode.fromJson(l))
+      .toList()
+    ..removeWhere((node) => node.type is! DeMucPhapdienNodeType);
+
+  List<String> ids = [];
+
+  for (final node in phapdienNodes) {
+    ids.add(node.id);
+    File file = File('crawl_data/${node.id}.json');
+    final content = await http.get(Uri.parse('$sourceUrl/v0/phapdien/demuc_content?id=${phapdienNodes.first.id}'));
+    await file.writeAsString(content.body);
+  }
+
+  File file = File('crawl_data/ids.json');
+  await file.writeAsString(json.encode(ids));
+}

b639958acc49a86e30a75246571540e0f28de529
Definev <daiduong.workmail@gmail.com>
2023-12-03 00:26:54 +0700

    Update directory path in regenerate_ids.dart

--- server/bin/regenerate_ids.dart
+++ server/bin/regenerate_ids.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  final dir = Directory('crawl_data');
+  final dir = Directory('crawl_data/raw');
   final entities = dir.listSync();
   final ids = <String>[];
   for (final entity in entities.whereType<File>()) {

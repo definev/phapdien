1abdd3cd90d89f9625c3f3b39071ab917b8eaf60
tripercy <percytri37@gmail.com>
2023-12-06 16:02:30 +0700

    Refactor chat_api.dart for improved readability and performance

--- server/lib/v0/routes/chat/chat_api.dart
+++ server/lib/v0/routes/chat/chat_api.dart
@@ -10,12 +10,23 @@ Future<Response> askHandler(Request req) async {
   int index = 0;
 
   final keys = providerContainer.read(environmentProvider).openAIKeys;
-  final query = (json.decode(await req.readAsString()) as Map<String, dynamic>)['query'] as String; 
+  final query = (json.decode(await req.readAsString())
+      as Map<String, dynamic>)['query'] as String;
 
-  final controller = providerContainer.read(getPhapdienSearchControllerProvider);
+  final controller =
+      providerContainer.read(getPhapdienSearchControllerProvider);
   final result = await controller.searchByQuery(query, 5);
-  
-  
+
+  var context = '';
+  var maxWords = 500;
+
+  for (final item in result) {
+    context += '${item.demucTitle}:\n${item.content}\n\n';
+    maxWords -= item.content.split(' ').length;
+    if (maxWords <= 0) {
+      break;
+    }
+  }
 
   while (true) {
     try {
@@ -29,21 +40,25 @@ Future<Response> askHandler(Request req) async {
               content: '''
 Bạn là một trợ lý tìm kiếm thông tin trong văn bản được cung cấp.
 Tìm thông tin trong văn bản được cung cấp để trả lời câu hỏi.
-Nếu câu hỏi không liên quan đến văn bản được cung cấp, nói "Câu hỏi không liên quan"''',
+Nếu câu hỏi không liên quan đến văn bản được cung cấp, nói "Câu hỏi không liên quan". 
+Chỉ dùng thông tin được cung cấp trong Tài liệu.''',
             ),
             ChatCompletionMessage.user(
-              content: ChatCompletionUserMessageContent.string("Tài liệu: $query"),
+              content: ChatCompletionUserMessageContent.string(
+                  "Tài liệu:\n$context"),
             ),
             ChatCompletionMessage.user(
-              content: ChatCompletionUserMessageContent.string("Câu hỏi: $query"),
+              content:
+                  ChatCompletionUserMessageContent.string("Câu hỏi: $query"),
             ),
           ],
           temperature: 0.7,
         ),
       );
-      print(res.choices.first.message.content);
 
-      return Response.ok(res.choices.first.message.content);
+      final answer = res.choices.first.message.content.toString();
+      print(answer);
+      return Response.ok(answer);
     } catch (error) {
       print(error);
       index++;

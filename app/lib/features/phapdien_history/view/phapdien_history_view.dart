0db67652ab04d1f0b28f65326553fbcea236ba4c
Definev <daiduong.workmail@gmail.com>
2023-12-07 04:21:47 +0700

    Add deletePhapdienHistory functionality

--- app/lib/features/phapdien_history/view/phapdien_history_view.dart
+++ app/lib/features/phapdien_history/view/phapdien_history_view.dart
@@ -1,14 +1,16 @@
 import 'package:app/features/phapdien_chat/view/phapdien_chat_view.dart';
+import 'package:app/features/phapdien_history/providers/delete_phapdien_history.dart';
 import 'package:app/features/phapdien_history/providers/get_phapdien_history_list.dart';
-import 'package:app/features/phapdien_history/repository/phapdien_history_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 class PhapdienHistoryView extends ConsumerWidget {
   const PhapdienHistoryView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
     final navigator = Navigator.of(context);
 
     final list = ref.watch(getPhapdienHistoryListProvider);
@@ -26,13 +28,20 @@ class PhapdienHistoryView extends ConsumerWidget {
                     builder: (context) => PhapdienChatView(history: item),
                   ),
                 ),
-                title: switch (list.firstOrNull) {
-                  null => const Text('Không có tiêu đề'),
-                  final first => Text(first.messages.firstOrNull?.question ?? 'Không có tiêu đề'),
-                },
+                title: DefaultTextStyle(
+                  style: theme.textTheme.titleMedium!.copyWith(
+                    color: theme.colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  child: switch (list.firstOrNull) {
+                    null => const Text('Không có tiêu đề'),
+                    final first => Text(first.messages.firstOrNull?.question ?? 'Không có tiêu đề'),
+                  },
+                ),
+                subtitle: Text('Cập nhât gần nhất: ${DateFormat('dd/MM/yyyy HH:mm').format(item.updatedAt)}'),
                 trailing: IconButton(
-                  icon: const Icon(Icons.chevron_right),
-                  onPressed: () => ref.read(phapdienHistoryRepositoryProvider).deletePhapdienHistory(item.id),
+                  icon: const Icon(Icons.delete),
+                  onPressed: () => ref.read(deletePhapdienHistoryProvider(item)),
                 ),
               );
             },

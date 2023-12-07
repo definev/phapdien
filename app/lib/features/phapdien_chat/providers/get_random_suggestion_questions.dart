b9eb4169efdd160e5c2bd5208247d7af28d97814
Definev <daiduong.workmail@gmail.com>
2023-12-07 13:24:23 +0700

    remove hardcode

--- app/lib/features/phapdien_chat/providers/get_random_suggestion_questions.dart
+++ app/lib/features/phapdien_chat/providers/get_random_suggestion_questions.dart
@@ -6,8 +6,5 @@ part 'get_random_suggestion_questions.g.dart';
 @riverpod
 Future<List<String>> getRandomSuggestionQuestions(GetRandomSuggestionQuestionsRef ref) async {
   final repository = ref.watch(phapdienChatRepositoryProvider);
-  return [
-    ...(await repository.getRandomSuggestionQuestions()),
-    'Quy định về quản lý sử dụng vũ khí vật liệu nổ tại việt nam?'
-  ];
+  return repository.getRandomSuggestionQuestions();
 }
